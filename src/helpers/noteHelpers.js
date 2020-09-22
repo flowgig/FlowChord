5e86918a14e23b49e10b0c2ad028aeb014e3a032
Benjamin Dehli <superelg@gmail.com>
2020-09-23 00:02:44 +0200

    Add helper functions

--- src/helpers/noteHelpers.js
+++ src/helpers/noteHelpers.js
@@ -3,3 +3,21 @@ export const getNoteByNoteNumber = (notes, noteNumber) => {
     return note.number === noteNumber;
   })
 }
+
+export const noteNumberToHalfSteps = (noteNumber, selectedKeyNumber) => {
+  return (noteNumber - selectedKeyNumber) % 12 >= 0 ? (noteNumber - selectedKeyNumber) % 12 : (12 + noteNumber - selectedKeyNumber) % 12;
+}
+
+export const noteNumbersToHalfSteps = (noteNumbers, selectedKeyNumber) => {
+  return noteNumbers.map(noteNumber => {
+    return noteNumberToHalfSteps(noteNumber, selectedKeyNumber);
+  });
+}
+
+export const getSelectedNoteNumbersFromNotes = notes => {
+  return notes.filter(note => {
+    return note.selected;
+  }).map(note => {
+    return note.number;
+  });
+}
