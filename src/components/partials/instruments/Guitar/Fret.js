61cb9877f1d332e3e209e887cedac5038ab031d8
Benjamin Dehli <superelg@gmail.com>
2020-09-25 00:25:55 +0200

    Use note component in frets

--- src/components/partials/instruments/Guitar/Fret.js
+++ src/components/partials/instruments/Guitar/Fret.js
@@ -3,9 +3,8 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
-import Button from '@material-ui/core/Button';
-import { ThemeProvider } from '@material-ui/core/styles';
-
+// Components
+import Note from 'components/partials/Note';
 
 // Actions
 import {toggleNote} from 'actions/NotesActions';
@@ -13,69 +12,21 @@ import {updateSelectedChordNameFromNotes} from 'actions/SelectedChordNameActions
 
 // Stylesheets
 import style from 'components/partials/instruments/Guitar/Fret.module.scss';
-import markerStyle from 'style/template/markers.module.scss';
-
-import {markerColorThemes} from 'theme';
 
 
 class Fret extends Component {
-  componentDidUpdate(prevProps){
-  }
 
-  getHalfSteps(fretNumber, tunerNumber, selectedKeyNumber){
-		let halfSteps = null;
-		if((fretNumber + tunerNumber - selectedKeyNumber - 1) % 12 >= 0){
-			halfSteps = (fretNumber + tunerNumber - selectedKeyNumber - 1) % 12;
-		}else{
-			halfSteps = (12 + fretNumber + tunerNumber - selectedKeyNumber - 1) % 12;
-		}
-		return halfSteps;
-	}
 	getKeyNumber(fretNumber, tunerNumber){
 		return (fretNumber + tunerNumber - 1) % 12;
 	}
-	getKeyName(keyNumber){
-		return this.props.notes[keyNumber].name;
-	}
-
-  handleFretOnChange(checked, noteNumber, selectedKeyNumber, chords){
-    const newNotes = this.props.toggleNote(noteNumber, checked);
-    this.props.updateSelectedChordNameFromNotes(newNotes, selectedKeyNumber, chords);
-  }
 
   render() {
-    const inputId = `select-interval-guitar-${this.props.stringNumber}-${this.props.fretNumber}`;
     const keyNumber = this.getKeyNumber(this.props.fretNumber, this.props.tunerNumber);
-    const keyName = this.getKeyName(keyNumber);
     const note = this.props.notes[keyNumber];
-    const halfSteps = this.getHalfSteps(this.props.fretNumber, this.props.tunerNumber, this.props.selectedKeyNumber);
-    const markerClass = note.selected ? `${markerStyle.marker} ${markerStyle.markerColor} ${markerStyle[halfSteps]}` : `${markerStyle.marker} ${markerStyle.noMarker}`;
-    const markerColor = note.selected ? `palette.markerColor${halfSteps}` : `default`;
-    const label = () => {
-      switch(this.props.selectedLabel) {
-        case 'key':
-          return keyName;
-        case 'interval':
-          return this.props.note.interval;
-        case 'halfSteps':
-          return halfSteps;
-        default:
-          return '';
-      }
-    }
-    const noteSelection = this.props.selectionType === 'chord' ? this.props.chords : this.props.scales;
 
     return (<div className={style.fret}>
-			<label htmlFor={inputId}>
-				<span>
-        <ThemeProvider theme={markerColorThemes[halfSteps]}>
-          <Button color="primary" disableElevation variant={note.selected ? 'contained' : 'text'} onClick={event => this.handleFretOnChange(!note.selected, keyNumber, this.props.selectedKeyNumber, noteSelection)}>{label()}</Button>
-        </ThemeProvider>
-				</span>
-			</label>
-
-
-	</div>)
+        <Note note={note} keyNumber={keyNumber} />
+      </div>)
   }
 }
 
@@ -87,12 +38,6 @@ Fret.propTypes = {
 
 const mapStateToProps = state => ({
   notes: state.notes,
-  chords: state.chords,
-  scales: state.scales,
-  selectionType: state.selectionType,
-  selectedLabel: state.selectedLabel,
-  selectedHalfSteps: state.selectedHalfSteps,
-  selectedKeyNumber: state.selectedKeyNumber
 });
 
 const mapDispatchToProps = {
