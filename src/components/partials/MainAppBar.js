8c293dc5e497b1bacf43254b75a05ffa5e0e94dd
Benjamin Dehli <superelg@gmail.com>
2020-09-26 00:06:28 +0200

    Add Selection component to Main App Bar

--- src/components/partials/MainAppBar.js
+++ src/components/partials/MainAppBar.js
@@ -1,5 +1,5 @@
 // Dependencies
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import clsx from 'clsx';
 
@@ -26,6 +26,7 @@ import Typography from '@material-ui/core/Typography';
 
 // Components
 //import { mainListItems, secondaryListItems } from 'components/ListItems';
+import Selection from 'components/partials/Selection';
 import SideBar from 'components/partials/SideBar';
 
 // Stylesheets
@@ -52,28 +53,28 @@ class MainAppBar extends Component {
     this.handleLogOut = this.handleLogOut.bind(this);
   }
 
-  handleDrawerOpen(){
-    this.setState({
-      isDrawerOpen: true
-    });
+  handleDrawerOpen() {
+    this.setState({isDrawerOpen: true});
   };
-  handleDrawerClose(){
-    this.setState({
-      isDrawerOpen: false
-    });
+  handleDrawerClose() {
+    this.setState({isDrawerOpen: false});
   };
 
-  setAnchorEl(element){
+  setAnchorEl(element) {
     this.setState({
       anchorEl: element,
-      isMenuOpen: element ? true : false
+      isMenuOpen: element
+        ? true
+        : false
     });
   }
 
-  setMobileMoreAnchorEl(element){
+  setMobileMoreAnchorEl(element) {
     this.setState({
       mobileMoreAnchorEl: element,
-      isMobileMenuOpen: element ? true : false
+      isMobileMenuOpen: element
+        ? true
+        : false
     });
   }
 
@@ -95,150 +96,115 @@ class MainAppBar extends Component {
   }
 
   handleLogOut() {
-  //  this.props.logOut();
+    //  this.props.logOut();
   }
 
-  renderMenu(){
-    return (
-      <Menu
-        anchorEl={this.state.anchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        id={this.state.menuId}
-        keepMounted
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={this.state.isMenuOpen}
-        onClose={this.handleMenuClose}
-        >
-        <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
-        <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
-        <MenuItem onClick={this.handleLogOut}>Log out</MenuItem>
-      </Menu>
-    );
+  renderMenu() {
+    return (<Menu anchorEl={this.state.anchorEl} anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }} id={this.state.menuId} keepMounted={true} transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }} open={this.state.isMenuOpen} onClose={this.handleMenuClose}>
+      <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
+      <MenuItem onClick={this.handleLogOut}>Log out</MenuItem>
+    </Menu>);
   }
 
-  renderMobileMenu(){
-    return (
-      <Menu
-        anchorEl={this.state.mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        id={this.state.mobileMenuId}
-        keepMounted
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={this.state.isMobileMenuOpen}
-        onClose={this.handleMobileMenuClose}
-        >
-        <MenuItem>
-          <IconButton aria-label="Show 4 new mails" color="inherit">
-            <Badge badgeContent={4} color="primary">
-              <MailIcon />
-            </Badge>
-          </IconButton>
-          <p>Messages</p>
-        </MenuItem>
-        <MenuItem>
-          <IconButton aria-label="Show 11 new notifications" color="inherit">
-            <Badge badgeContent={11} color="primary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <p>Notifications</p>
-        </MenuItem>
-        <MenuItem onClick={this.handleProfileMenuOpen}>
-          <IconButton
-            aria-label="Account of current user"
-            aria-controls="primary-search-account-menu"
-            aria-haspopup="true"
-            color="inherit"
-            >
-            <AccountCircle />
-          </IconButton>
-          <p>Profile</p>
-        </MenuItem>
-      </Menu>
-    );
+  renderMobileMenu() {
+    return (<Menu anchorEl={this.state.mobileMoreAnchorEl} anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }} id={this.state.mobileMenuId} keepMounted={true} transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }} open={this.state.isMobileMenuOpen} onClose={this.handleMobileMenuClose}>
+      <MenuItem>
+        <IconButton aria-label="Show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="primary">
+            <MailIcon/>
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="Show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="primary">
+            <NotificationsIcon/>
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={this.handleProfileMenuOpen}>
+        <IconButton aria-label="Account of current user" aria-controls="primary-search-account-menu" aria-haspopup="true" color="inherit">
+          <AccountCircle/>
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>);
   }
 
   render() {
-    return (
-      <div className={styles.grow}>
-        <AppBar position="absolute" className={clsx(styles.appBar, this.state.isDrawerOpen && styles.appBarShift)}>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              className={clsx(styles.menuButton, this.state.isDrawerOpen && styles.menuButtonHidden)}
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              >
-              <MenuIcon />
+    return (<div className={styles.grow}>
+      <AppBar position="absolute" className={clsx(styles.appBar, this.state.isDrawerOpen && styles.appBarShift)}>
+        <Toolbar>
+          <IconButton edge="start" className={clsx(styles.menuButton, this.state.isDrawerOpen && styles.menuButtonHidden)} color="inherit" aria-label="Open drawer" onClick={this.handleDrawerOpen}>
+            <MenuIcon/>
+          </IconButton>
+          <Typography className={styles.title} variant="h6" noWrap={true}>
+            FlowChord
+          </Typography>
+          <div className={styles.grow}/>
+          <Selection/>
+          <div className={styles.grow}/>
+
+          <div className={styles.sectionDesktop}>
+            <IconButton aria-label="Show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="primary">
+                <MailIcon/>
+              </Badge>
             </IconButton>
-            <Typography className={styles.title} variant="h6" noWrap>
-              FlowChord
-            </Typography>
-            <div className={styles.grow} />
-            <div className={styles.sectionDesktop}>
-              <IconButton aria-label="Show 4 new mails" color="inherit">
-                <Badge badgeContent={4} color="primary">
-                  <MailIcon />
-                </Badge>
-              </IconButton>
-              <IconButton aria-label="Show 17 new notifications" color="inherit">
-                <Badge badgeContent={17} color="primary">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                edge="end"
-                aria-label="Account of current user"
-                aria-controls={this.state.menuId}
-                aria-haspopup="true"
-                onClick={this.handleProfileMenuOpen}
-                color="inherit"
-                >
-                <AccountCircle />
-              </IconButton>
-            </div>
-            <div className={styles.sectionMobile}>
-              <IconButton
-                aria-label="Show more"
-                aria-controls={this.state.mobileMenuId}
-                aria-haspopup="true"
-                onClick={this.handleMobileMenuOpen}
-                color="inherit"
-                >
-                <MoreIcon />
-              </IconButton>
-            </div>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          classes={{
-            paper: clsx(styles.drawerPaper, !this.state.isDrawerOpen && styles.drawerPaperClose),
-          }}
-          open={this.state.isDrawerOpen}
-          >
-          <div className={styles.toolbarIcon}>
-            <IconButton onClick={this.handleDrawerClose}>
-              <ChevronLeftIcon />
+            <IconButton aria-label="Show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="primary">
+                <NotificationsIcon/>
+              </Badge>
+            </IconButton>
+            <IconButton edge="end" aria-label="Account of current user" aria-controls={this.state.menuId} aria-haspopup="true" onClick={this.handleProfileMenuOpen} color="inherit">
+              <AccountCircle/>
             </IconButton>
           </div>
-          <Divider />
-          <List>
-            <SideBar />
-          </List>
-          <Divider />
-          <List></List>
-        </Drawer>
-        {this.renderMobileMenu()}
-        {this.renderMenu()}
-      </div>
-    );
+          <div className={styles.sectionMobile}>
+            <IconButton aria-label="Show more" aria-controls={this.state.mobileMenuId} aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
+              <MoreIcon/>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" classes={{
+          paper: clsx(styles.drawerPaper, !this.state.isDrawerOpen && styles.drawerPaperClose)
+        }} open={this.state.isDrawerOpen}>
+        <div className={styles.toolbarIcon}>
+          <IconButton onClick={this.handleDrawerClose}>
+            <ChevronLeftIcon/>
+          </IconButton>
+        </div>
+        <Divider/>
+        <List>
+          <SideBar/>
+        </List>
+        <Divider/>
+        <List></List>
+      </Drawer>
+      {this.renderMobileMenu()}
+      {this.renderMenu()}
+    </div>);
   }
 }
 
 const mapDispatchToProps = {
-//  logOut
+  //  logOut
 };
 
 export default connect(null, mapDispatchToProps)(MainAppBar);
