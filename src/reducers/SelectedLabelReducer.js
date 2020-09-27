4dafb153df18dcc9d5750ff4f1d7621f0457ceaa
Benjamin Dehli <superelg@gmail.com>
2020-09-27 20:08:57 +0200

    Add switch case for selected label reducer

--- src/reducers/SelectedLabelReducer.js
+++ src/reducers/SelectedLabelReducer.js
@@ -1,8 +1,12 @@
-const initialState = "key";
+import { UPDATE_SELECTED_LABEL } from 'constants/types';
+
+const initialState = 'key';
 
 export default function(state = initialState, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
+	switch(action.type) {
+		case UPDATE_SELECTED_LABEL:
+			return action.payload;
+		default:
+			return state;
+	}
 }
