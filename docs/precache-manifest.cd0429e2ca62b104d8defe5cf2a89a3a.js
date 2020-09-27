4b76a6243c7b62ad4da915b55e016c37bd2a979a
Benjamin Dehli <superelg@gmail.com>
2020-09-27 21:44:34 +0200

    Adjust public url variable

---
+++ docs/precache-manifest.cd0429e2ca62b104d8defe5cf2a89a3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7756de5ce2eaf59621d9fabd887ddfd6",
+    "url": "/FlowChord/index.html"
+  },
+  {
+    "revision": "aa54b1599475cb4694db",
+    "url": "/FlowChord/static/css/main.22b9380f.chunk.css"
+  },
+  {
+    "revision": "65753c9c78b0972a32f5",
+    "url": "/FlowChord/static/js/2.51ade638.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/FlowChord/static/js/2.51ade638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa54b1599475cb4694db",
+    "url": "/FlowChord/static/js/main.25c45c0f.chunk.js"
+  },
+  {
+    "revision": "4f98878457701c8b4638",
+    "url": "/FlowChord/static/js/runtime-main.eb77657a.js"
+  },
+  {
+    "revision": "be6822f1457dfa8b92eddc5473e4b3bc",
+    "url": "/FlowChord/static/media/flowchord-logo-gray-vertical.be6822f1.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.618d58d59113b0d9c8f26b50efdd95dc.js"
+  "/FlowChord/precache-manifest.cd0429e2ca62b104d8defe5cf2a89a3a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FlowChord/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
