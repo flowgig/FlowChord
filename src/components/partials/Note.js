3864df5d6d91156a3c94a89a71760f6745e32cd8
Benjamin Dehli <superelg@gmail.com>
2020-09-27 20:07:12 +0200

    Replace Theme Provider styles with local styles for notes

--- src/components/partials/Note.js
+++ src/components/partials/Note.js
@@ -5,30 +5,24 @@ import PropTypes from 'prop-types';
 
 // Material UI
 import Button from '@material-ui/core/Button';
-import { ThemeProvider } from '@material-ui/core/styles';
 
 // Actions
 import {toggleNote} from 'actions/NotesActions';
 import {updateSelectedSelectionNameFromNotes} from 'actions/SelectedSelectionNameActions';
 
+// Helpers
+import {noteNumberToHalfSteps} from 'helpers/noteHelpers';
+
 // Stylesheets
 import style from 'components/partials/Note.module.scss';
-import {markerColorThemes} from 'theme';
-
 
 class Note extends Component {
-  getHalfSteps(keyNumber, selectedKeyNumber){
-		let halfSteps = null;
-		if((keyNumber - selectedKeyNumber) % 12 >= 0){
-			halfSteps = (keyNumber - selectedKeyNumber) % 12;
-		}else{
-			halfSteps = (12 + keyNumber - selectedKeyNumber) % 12;
-		}
-		return halfSteps;
-	}
-
 	getKeyName(keyNumber){
 		return this.props.notes[keyNumber].name;
+	}
+
+  getInterval(halfSteps){
+		return this.props.intervals[halfSteps];
 	}
 
   handleNoteOnClick(checked, noteNumber, selectedKeyNumber, noteSelections, selectedSelectionType){
@@ -39,15 +33,16 @@ class Note extends Component {
   render() {
     const keyNumber = this.props.keyNumber;
     const keyName = this.getKeyName(keyNumber);
-    const note = this.props.notes[keyNumber];
-    const halfSteps = this.getHalfSteps(keyNumber, this.props.selectedKeyNumber);
+    const note = this.props.note;
+    const halfSteps = noteNumberToHalfSteps(keyNumber, this.props.selectedKeyNumber);
+    const interval = this.getInterval(halfSteps);
 
     const label = () => {
       switch(this.props.selectedLabel) {
         case 'key':
           return keyName;
         case 'interval':
-          return this.props.note.interval;
+          return interval;
         case 'halfSteps':
           return halfSteps;
         default:
@@ -57,22 +52,29 @@ class Note extends Component {
     const noteSelections = this.props.selectedSelectionType === 'scale' ? this.props.scales : this.props.chords;
 
     return (<div className={style.note}>
-        <ThemeProvider theme={markerColorThemes[halfSteps]}>
-          <Button color="primary" disableElevation variant={note.selected ? 'contained' : 'text'} onClick={event => this.handleNoteOnClick(!note.selected, keyNumber, this.props.selectedKeyNumber, noteSelections, this.props.selectedSelectionType)}>{label()}</Button>
-        </ThemeProvider>
+          <Button color="default"
+                  disableElevation
+                  variant={note.selected ? 'contained' : 'text'}
+                  onClick={event => this.handleNoteOnClick(!note.selected, keyNumber, this.props.selectedKeyNumber, noteSelections, this.props.selectedSelectionType)}
+                  className={`${style.button} ${note.selected ? style[halfSteps] : style.notSelected} ${this.props.keyColor ? style[this.props.keyColor] : ''}`}
+                  >
+                    {label()}
+          </Button>
 	</div>)
   }
 }
 
 Note.propTypes = {
   note: PropTypes.object,
-  keyNumber: PropTypes.number
+  keyNumber: PropTypes.number,
+	keyColor: PropTypes.string
 }
 
 const mapStateToProps = state => ({
   notes: state.notes,
   chords: state.chords,
   scales: state.scales,
+  intervals: state.intervals,
   selectedSelectionType: state.selectedSelectionType,
   selectedLabel: state.selectedLabel,
   selectedKeyNumber: state.selectedKeyNumber
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,3 @@
-import { red } from '@material-ui/core/colors';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 // A custom theme for this app
@@ -10,47 +9,8 @@ const theme = createMuiTheme({
     secondary: {
       main: '#19857b',
     },
-    error: {
-      main: red.A400,
-    },
     background: {
       default: '#fff',
-    },
-    markerColor0: {
-      main: '#2274d8'
-    },
-    markerColor1: {
-      main: '#03AFBF'
-    },
-    markerColor2: {
-      main: '#1CC43B'
-    },
-    markerColor3: {
-      main: '#ED9D07'
-    },
-    markerColor4: {
-      main: '#f3382b'
-    },
-    markerColor5: {
-      main: '#841726'
-    },
-    markerColor6: {
-      main: '#512DA8'
-    },
-    markerColor7: {
-      main: '#CD22D8'
-    },
-    markerColor8: {
-      main: '#C2185B'
-    },
-    markerColor9: {
-      main: '#388E3C'
-    },
-    markerColor10: {
-      main: '#00796'
-    },
-    markerColor11: {
-      main: '#90521'
     }
   },
   overrides: {
@@ -61,21 +21,5 @@ const theme = createMuiTheme({
     },
   },
 });
-
-const markerColorCodes = ['#2274d8','#03AFBF','#1CC43B','#ED9D07','#f3382b','#841726','#512DA8','#CD22D8','#C2185B','#388E3C','#00796B','#90521e'];
-
-const markerColorThemes = markerColorCodes.map(colorCode => {
-  const markerTheme = createMuiTheme({
-    palette: {
-      primary: {main: colorCode},
-    },
-  });
-  return {
-    palette: markerTheme.palette
-  };
-})
-
-
-export {markerColorThemes};
 
 export default theme;
