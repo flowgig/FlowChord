dbfad3dcab9b3a1b65c91ff5f6c9ccd2ea961d5b
Benjamin Dehli <superelg@gmail.com>
2021-01-14 21:59:43 +0100

    Fix lint warnings for reducer

--- src/reducers/SynthesizerReducer.js
+++ src/reducers/SynthesizerReducer.js
@@ -1,12 +1,12 @@
 import * as Tone from 'tone';
 
-const synthesizer = new Tone.PolySynth(Tone.Synth).toDestination();
+const initialState = new Tone.PolySynth(Tone.Synth).toDestination();;
 
-const initialState = synthesizer;
-
-export default function(state = initialState, action) {
-	switch(action.type) {
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
 		default:
 			return state;
 	}
 }
+
+export default reducer;
