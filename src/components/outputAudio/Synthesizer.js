01c4f105fd4612411faf299316c7cd95bf1f076b
Benjamin Dehli <superelg@gmail.com>
2021-08-17 23:49:41 +0200

    Adjust playback and selection logic for synthesizer

--- src/components/outputAudio/Synthesizer.js
+++ src/components/outputAudio/Synthesizer.js
@@ -1,41 +1,20 @@
 // Dependencies
-import {
-  Component
-} from 'react';
-import {
-  connect
-} from 'react-redux';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import * as Tone from 'tone';
+
+// Material UI
+import Fab from '@material-ui/core/Fab';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 // Helpers
-import {
-  getNoteByNoteNumber
-} from 'helpers/noteHelpers';
+import { getNoteByNoteNumber } from 'helpers/noteHelpers';
 
 
 class Synthesizer extends Component {
-
-  componentDidUpdate(prevProps) {
-    const chordHasChanged = this.props.selectedChordName !== prevProps.selectedChordName;
-    const scaleHasChanged = this.props.selectedScaleName !== prevProps.selectedScaleName;
-    const keyHasChanged = this.props.selectedKeyNumber !== prevProps.selectedKeyNumber;
-
-    const synthesizer = this.props.synthesizer;
-    const notes = this.props.notes;
-    const selectedSelections = this.props.selectedSelectionType === 'scale' ? this.props.scales : this.props.chords;
-    const selectedSelectionName = this.props.selectedSelectionType === 'scale' ? this.props.selectedScaleName : this.props.selectedChordName;
-
-
-    if (selectedSelectionName && selectedSelectionName !== 'custom') {
-      const selectedSelection = selectedSelections[selectedSelectionName];
-      const selectedHalfSteps = selectedSelection.halfSteps;
-      const selectedNotesTriggerNames = this.getSelectedNotesTriggerNames(selectedHalfSteps, this.props.selectedKeyNumber, notes);
-
-      if (chordHasChanged || (keyHasChanged && this.props.selectedSelectionType === 'chord')) {
-        synthesizer.triggerAttackRelease(selectedNotesTriggerNames, 0.5);
-      } else if (scaleHasChanged || (keyHasChanged && this.props.selectedSelectionType === 'scale')) {
-
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.playSelection = this.playSelection.bind(this);
   }
 
   getSelectedNotesTriggerNames(halfSteps, selectedKeyNumber, notes) {
@@ -49,15 +28,53 @@ class Synthesizer extends Component {
     })
   };
 
+  playSelection() {
+    const synthesizer = this.props.synthesizer;
+    const notes = this.props.notes;
+    const selectedSelections = this.props.selectedSelectionType === 'scale' ? this.props.scales : this.props.chords;
+    const selectedSelectionName = this.props.selectedSelectionType === 'scale' ? this.props.selectedScaleName : this.props.selectedChordName;
+    if (selectedSelectionName) {
+      const selectedSelection = selectedSelections[selectedSelectionName];
+      const selectedHalfSteps = selectedSelection.halfSteps;
+      const selectedNotesTriggerNames = this.getSelectedNotesTriggerNames(selectedHalfSteps, this.props.selectedKeyNumber, notes);
+      console.log(selectedNotesTriggerNames)
+      const now = Tone.now()
+
+      if (this.props.selectedSelectionType === 'chord') {
+        // Trigger simultaneously
+        synthesizer.triggerAttackRelease(selectedNotesTriggerNames, 0.5);
+        // Trigger consecutively
+        selectedNotesTriggerNames.forEach((selectedNotesTriggerName, index) => {
+          const noteLength = 0.25;
+          const noteStart = now + 0.75 + 0.25 * index;
+          synthesizer.triggerAttackRelease(selectedNotesTriggerName, noteLength, noteStart);
+        })
+      } else if (this.props.selectedSelectionType === 'scale') {
+        // Trigger consecutively
+        selectedNotesTriggerNames.forEach((selectedNotesTriggerName, index) => {
+          const noteLength = 0.25;
+          const noteStart = now + 0.25 * index;
+          synthesizer.triggerAttackRelease(selectedNotesTriggerName, noteLength, noteStart);
+        })
+      }
+
+    }
+
+  }
+
   render() {
-    return null
+    return (
+      <Fab color="primary" aria-label={`Listen to ${this.props.selectedSelectionType}`} onClick={this.playSelection}>
+        <PlayArrowIcon />
+      </Fab>
+    )
   }
 }
 
 const mapStateToProps = state => ({
   notes: state.notes,
   chords: state.chords,
-  scales: state.scale,
+  scales: state.scales,
   selectedChordName: state.selectedChordName,
   selectedScaleName: state.selectedScaleName,
   selectedKeyNumber: state.selectedKeyNumber,
