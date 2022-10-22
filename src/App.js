19b2e9f356ced4ad080979b684413e0eef5013cf
Benjamin Dehli <superelg@gmail.com>
2022-10-23 00:55:06 +0200

    Upgrade to React 18

--- src/App.js
+++ src/App.js
@@ -1,64 +1,63 @@
 // Dependecies
-import React, { Component } from 'react';
-import {Provider} from 'react-redux';
-import {Route, Switch} from 'react-router';
-import {ConnectedRouter} from 'connected-react-router';
-import WebFont from 'webfontloader';
-import { ThemeProvider } from '@material-ui/styles';
+import React from "react";
+import { Provider } from "react-redux";
+import { Route, Routes } from "react-router";
+import { HistoryRouter as Router } from "redux-first-history/rr6";
+import WebFont from "webfontloader";
+import { ThemeProvider } from "@material-ui/styles";
 
 // Material UI
-import CssBaseline from '@material-ui/core/CssBaseline';
-
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 // Utils
-import configureStore, {history} from 'utils/configureStore';
+import configureStore, { history } from "utils/configureStore";
 
 // Routes
-import Home from 'components/routes/Home';
-import NotFound from 'components/routes/NotFound';
+import Home from "components/routes/Home";
+import NotFound from "components/routes/NotFound";
 
 // Partials
-import MainAppBar from 'components/partials/MainAppBar';
-import Footer from 'components/partials/Footer';
-import FloatingActionButtons from 'components/partials/FloatingActionButtons';
+import MainAppBar from "components/partials/MainAppBar";
+import Footer from "components/partials/Footer";
+import FloatingActionButtons from "components/partials/FloatingActionButtons";
 
 // InputControllers
-import ComputerKeyboardInput from 'components/inputControllers/ComputerKeyboardInput';
+import ComputerKeyboardInput from "components/inputControllers/ComputerKeyboardInput";
 
 // Stylesheets
-import style from 'App.module.scss';
-import theme from 'theme';
+import style from "App.module.scss";
+import theme from "theme";
 
 WebFont.load({
-  google: {
-    families: ['Roboto:400,700&display=swap']
-  }
+    google: {
+        families: ["Roboto:400,700&display=swap"]
+    }
 });
 
 const initialState = {};
 const store = configureStore(initialState);
 
-class App extends Component {
-  render() {
-    return (<Provider store={store}>
-      <ConnectedRouter history={history}>
-        <CssBaseline />
-        <ThemeProvider theme={theme}>
-          <ComputerKeyboardInput />
-          <MainAppBar />
-          <div className={style.app}>
-            <Switch>
-              <Route exact={true} path="/" render={(props) => (<Home {...props}/>)}/>
-              <Route exact={true} path="/FlowChord" render={(props) => (<Home {...props}/>)}/>
-              <Route render={() => (<NotFound/>)}/>
-            </Switch>
-            <Footer />
-            <FloatingActionButtons />
-          </div>
-        </ThemeProvider>
-      </ConnectedRouter>
-    </Provider>);
-  }
-}
+const App = () => {
+    return (
+        <Provider store={store}>
+            <Router history={history}>
+                <CssBaseline />
+                <ThemeProvider theme={theme}>
+                    <ComputerKeyboardInput />
+                    <MainAppBar />
+                    <div className={style.app}>
+                        <Routes>
+                            <Route exact={true} path="/" element={<Home />} />
+                            <Route exact={true} path="/FlowChord" element={<Home />} />
+                            <Route element={() => (<NotFound />)} />
+                        </Routes>
+                        <Footer />
+                        <FloatingActionButtons />
+                    </div>
+                </ThemeProvider>
+            </Router>
+        </Provider>
+    );
+};
 
 export default App;
